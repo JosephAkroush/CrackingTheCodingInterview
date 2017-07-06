d93694429ef7c913fb8a03740c103f462a2f92b2
Joseph Akroush <akroushj@gmail.com>
2017-07-05 21:33:45 -0400

    Create Program.cs

---
+++ Linked Lists/Remove Duplicates/Program.cs
@@ -0,0 +1,59 @@
+public class Node
+{
+    public int Data { get; set; }
+    public Node Next { get; set; }
+}
+
+static void RemoveDuplicatesWithBuffer(Node head)
+{
+    if (head == null)
+    {
+        return;
+    }
+
+    List<int> buffer = new List<int>();
+    Node node = head;
+
+    while (node != null)
+    {
+        if (buffer.Contains(node.Next.Data))
+        {
+            node.Next = node.Next.Next;
+        }
+        else
+        {
+            buffer.Add(node.Next.Data);
+        }
+
+        node = node.Next;
+    }
+}
+
+static void RemoveDuplicatesWithoutBuffer(Node head)
+{
+    if (head == null)
+    {
+        return;
+    }
+
+    Node node = head;
+
+    while (node != null)
+    {
+        Node runner = node;
+
+        while (runner.Next != null)
+        {
+            if (node.Data == runner.Next.Data)
+            {
+                runner.Next = runner.Next.Next;
+            }
+            else
+            {
+                runner = runner.Next;
+            }
+        }
+
+        node = node.Next;
+    }
+}
