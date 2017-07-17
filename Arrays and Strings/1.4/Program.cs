95202be1a6b76b82eb0c22a8b982d2236effd55b
Joseph Akroush <JosephAkroush@users.noreply.github.com>
2017-07-16 23:34:10 -0400

    Update Program.cs
    inline implementation

--- Arrays and Strings/1.4/Program.cs
+++ Arrays and Strings/1.4/Program.cs
@@ -1,26 +1,35 @@
 static void Replace(char[] input, int length)
 {
-    char[] output = new char[length];
-    int i = 0;
-    int j = 0;
+    int spaces = 0;
+    int newLength = 0;
 
-    while (i < length && j < length)
+    for (int i = 0; i < length; i++)
     {
         if (input[i] == ' ')
         {
-            output[j] = '%';
-            output[j + 1] = '2';
-            output[j + 2] = '0';
-            j = j + 3;
+            spaces++;
+        }
+    }
+
+    newLength = length + spaces * 2;
+
+    for (int i = length - 1; i >= 0; i--)
+    {
+        if (input[i] == ' ')
+        {
+            input[newLength - 1] = '0';
+            input[newLength - 2] = '2';
+            input[newLength - 3] = '%';
+
+            newLength = newLength - 3;
         }
         else
         {
-            output[j] = input[i];
-            j++;
-        }
+            input[newLength - 1] = input[i];
 
-        i++;
+            newLength--;
+        }
     }
 
-    Console.WriteLine(new string(output));
+    Console.WriteLine(new string(input));
 }
